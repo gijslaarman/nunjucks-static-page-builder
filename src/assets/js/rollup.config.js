c7782d63137cf7b277a9c4c331898aaf9bc87f5e
gijslaarman <gijslaarman@gmail.com>
2020-05-11 20:17:01 +0200

    js now minifies

--- src/assets/js/rollup.config.js
+++ src/assets/js/rollup.config.js
@@ -1,6 +1,7 @@
 import alias from "@rollup/plugin-alias";
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser"
 
 const config = {
   input: "src/assets/js/index.js",
@@ -16,6 +17,7 @@ const config = {
     alias({
       entries: [{ find: "@components", replacement: "../../components" }],
     }),
+    terser()
   ],
 };
 
